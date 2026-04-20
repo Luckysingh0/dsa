45e009c7d5de60207c969b0f5682cdc8a766862a
Luckysingh0 <88560655+Luckysingh0@users.noreply.github.com>
2026-04-21 00:15:19 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (33.57%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -13,9 +13,7 @@ class Solution {
 public:
     void in(TreeNode* root,vector<int>&ans)
     {
-        if(root==NULL)
-        return;
-
+        if(!root) return;
         in(root->left,ans);
         ans.push_back(root->val);
         in(root->right,ans);
